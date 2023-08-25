cfe32feedc9457bb242077c833da363fd4ea4ba3
Rajeshwaran SLV <rajesh.s@hepl.com>
2023-08-25 17:34:03 +0530

    update: post data

--- src/Component/post/Post.js
+++ src/Component/post/Post.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import "./post.scss";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
@@ -6,53 +8,60 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
-import { useState } from "react";
-import Posts from "../posts/Posts";
 
-const Post = ({ post }) => {
+const Post = () => {
     const [commentOpen, setCommentOpen] = useState(false);
+    const [post, setPost] = useState(null); 
+    const [liked, setLiked] = useState(false);
 
-    //TEMPORARY
-    const liked = false;
+    
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1') 
+            .then(response => {
+                setPost(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching post:', error);
+            });
+    }, []);
 
     return (
         <div className="post">
-            <div className="container">
-                <div className="user">
-                    <div className="userInfo">
-                        <img src={Posts.profilePic} alt="" />
-                        <div className="details">
-                            <Link
-                                // to={`/profile/${post.userId}`}
-                                style={{ textDecoration: "none", color: "inherit" }}
-                            >
-                                {/* <span className="name">{post.name}</span> */}
-                            </Link>
-                            <span className="date">1 min ago</span>
+            {post && ( 
+                <div className="container">
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <div className="details">
+                                <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+                                 
+                                    <span className="name">{post.userId}</span>
+                                </Link>
+                                <span className="date">1 min ago</span>
+                            </div>
+                        </div>
+                        <MoreHorizIcon />
+                    </div>
+                    <div className="content">
+                        <p>{post.body}</p>
+                    </div>
+                    <div className="info">
+                        <div className="item">
+                            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+                            12 Likes
+                        </div>
+                        <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+                            <TextsmsOutlinedIcon />
+                            12 Comments
+                        </div>
+                        <div className="item">
+                            <ShareOutlinedIcon />
+                            Share
                         </div>
                     </div>
-                    <MoreHorizIcon />
+                    {commentOpen && <Comments />}
                 </div>
-                <div className="content">
-                    {/* <p>{post.desc}</p> */}
-                    {/* <img src={post.img} alt="" /> */}
-                </div>
-                <div className="info">
-                    <div className="item">
-                        {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-                        12 Likes
-                    </div>
-                    <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
-                        <TextsmsOutlinedIcon />
-                        12 Comments
-                    </div>
-                    <div className="item">
-                        <ShareOutlinedIcon />
-                        Share
-                    </div>
-                </div>
-                {commentOpen && <Comments />}
-            </div>
+            )}
         </div>
     );
 };
